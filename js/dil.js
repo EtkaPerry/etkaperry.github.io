e0c6d918a2679afa0ca3dd1a25482984fd605799
Etka <etka@cocukveegitimi.com>
2018-12-03 13:46:03 +0300

    edited js'

--- js/dil.js
+++ js/dil.js
@@ -6,12 +6,16 @@ var LanguageList = {
 //languages Objects
 var WORDS_TR = {
     "merhaba": "Merhaba!",
-    "ugras": "Şuan neler ile uğraşıyorum?"
+    "ugras": "Şuan neler ile uğraşıyorum?",
+    "tweet": "Bu websiteye bir bak!",
+    "android": "Android Uygulama:"
 };
 
 var WORDS_EN = {
     "merhaba": "Hello!",
-    "ugras": "Now what i am working on?"
+    "ugras": "What I am working on right now?",
+    "android": "Chech this website!",
+    "android": "Android Uygulama:"
 };
 
 
@@ -19,10 +23,10 @@ window.onload = initialize;
 
 function initialize() {
 
-    var $dropdown = $("#country_select");
+    var $dropdown = $("#ddList-dil_sec");
     $.each(LanguageList, function (key, value) {
         $dropdown.
-        append($("<option/>").val(key).text(value));
+        append($("<li/>").val(key).text(value));
     });
 
     loadsLanguage("TR");
